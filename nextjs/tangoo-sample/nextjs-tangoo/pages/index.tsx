472606c31f62aee1d0a76854ff443879ff2bccab
ri003074 <ri003074@gmail.com>
2020-12-13 21:02:32 +0900

    add

--- nextjs/tangoo-sample/nextjs-tangoo/pages/index.tsx
+++ nextjs/tangoo-sample/nextjs-tangoo/pages/index.tsx
@@ -19,12 +19,12 @@ export default function Home({ contents }) {
         <thead>
           {contents.map((content,index) =>
             <tr key={content.id}>
-              <td>{index}</td>
-              <td>{content.phrase_en}</td>
-              <td>{content.phrase_ja}</td>
-              <td>{content.word_en}</td>
-              <td>{ ((content.c_counter / content.s_counter)*100).toFixed(0) } %</td>
-              <td>[x]</td>
+              <td style={{width:'10%', textAlign:'center'}}>{index}</td>
+              <td style={{width:'30%'}}>{content.phrase_en}</td>
+              <td style={{width:'25%'}}>{content.phrase_ja}</td>
+              <td style={{width:'15%'}}>{content.word_en}</td>
+              <td style={{width:'10%'}}>{ ((content.c_counter / content.s_counter)*100).toFixed(0) } %</td>
+              <td style={{width:'10%'}}>[x]</td>
             </tr>
           )}
         </thead>
