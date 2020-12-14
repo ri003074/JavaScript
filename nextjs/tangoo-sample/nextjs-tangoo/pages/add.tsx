d8bee2a4f53ecf0c98ec41fc14b56eb6dd3e3471
ri003074 <ri003074@gmail.com>
2020-12-14 19:19:39 +0900

    add Form

--- nextjs/tangoo-sample/nextjs-tangoo/pages/add.tsx
+++ nextjs/tangoo-sample/nextjs-tangoo/pages/add.tsx
@@ -1,6 +1,12 @@
 import HeaderLayout from '../components/header'
 import Head from 'next/head'
+import {Form, Col, Button} from 'react-bootstrap'
 function Add(){
+
+    function clickTest(e){
+        console.log("hehe")
+        console.log(e)
+    }
     return (
         <div>
             <Head>
@@ -8,6 +14,21 @@ function Add(){
             </Head>
 
             <HeaderLayout/>
+
+            <Form.Group>
+                <Col sm="4" className="mb-2">
+                    <Form.Control size="sm" type="text" placeholder="English Phrase" />
+                </Col>
+                <Col sm="4" className="mb-2">
+                    <Form.Control size="sm" type="text" placeholder="Japanese Phrase" />
+                </Col>
+                <Col sm="4">
+                    <Form.Control size="sm" type="text" placeholder="English Word" />
+                </Col>
+            </Form.Group>
+            <Button className="ml-3" variant="primary" type="submit" onClick={clickTest}>
+                Submit
+            </Button>
         </div>
     )
 }
