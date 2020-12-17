90b2b32bb4485b663e6587431d645311856f7aba
ri003074 <ri003074@gmail.com>
2020-12-18 07:13:05 +0900

    type and clear blank ok

--- react/react-typing-game/react-typing/src/components/App.js
+++ react/react-typing-game/react-typing/src/components/App.js
@@ -6,30 +6,6 @@ import { correctType, nextQuiz, resetQuiz } from '../actions'
 
 class App extends Component {
 
-
-  // keyDown = (e) => {
-  //     console.log(e.key)
-  //     console.log(this.state.quizSet[this.state.quizNumber])
-
-  //     if (this.state.quizSet[this.state.quizNumber][this.state.wordLocation] === e.key) { //正解のケース
-  //         console.log("ok!")
-  //         const wordLocation = this.state.wordLocation
-  //         this.setState({ wordLocation: wordLocation + 1 }) //次のwordLocationへ行く。
-
-  //         if(this.state.wordLocation === this.state.quizSet[this.state.quizNumber].length){  //タイプしている位置がクイズの長さと一致した場合、次の単語に行くか、最初の単語に戻る.
-  //             const quizNumber = this.state.quizNumber 
-  //             if(quizNumber === this.state.quizSet.length-1){ //最初の単語に戻る
-  //                 this.setState({quizNumber:0}) 
-  //             } else { //次の単語に行く
-  //                 this.setState({quizNumber: quizNumber+1}) 
-  //         } 
-  //         this.setState({wordLocation: 0})
-  //     }
-  //   } else {
-  //       console.log("ng!")
-  //   }
-  // }
-
   keydown(event) {
       const actions = this.props
 
@@ -61,13 +37,13 @@ class App extends Component {
 
 
       <React.Fragment>
-          <div className="App">{props.quizSet[props.quizNumber]}-{props.wordLocation}</div>
+          <div className="App">{props.quizSetBlank[props.quizNumber]}-{props.wordLocation}</div>
       </React.Fragment>
     );
   }
 }
 
-const mapStateToProps = state => ({ quizSet: state.quiz.quizSet,quizNumber: state.quiz.quizNumber, wordLocation: state.quiz.wordLocation }) 
+const mapStateToProps = state => ({ quizSet: state.quiz.quizSet, quizSetBlank:state.quiz.quizSetBlank, quizNumber: state.quiz.quizNumber, wordLocation: state.quiz.wordLocation }) 
 const mapDispatchToProps = dispatch => ({
   correctType: () => dispatch(correctType()),
   nextQuiz: () => dispatch(nextQuiz()),
--- react/react-typing-game/react-typing/src/reducers/quiz.js
+++ react/react-typing-game/react-typing/src/reducers/quiz.js
@@ -1,19 +1,32 @@
 import { CORRECTTYPE, NEXTQUIZ, RESETQUIZ } from '../actions'
 
 const initialState = {
-    quizSet: ["abc", "def"],
+    quizSet: ["abcdef", "defghi"],
+    quizSetBlank: ["a_____", "d_____"],
     quizNumber: 0,
-    wordLocation: 0,
+    wordLocation: 1,
 }
 
+
 export default (state = initialState, action) => {
+    function clearQuizSetBlank() {
+        state.quizSetBlank[state.quizNumber] = state.quizSet[state.quizNumber].substring(0, 1) + '_'.repeat(state.quizSet[state.quizNumber].length - 1);
+    }
+
     switch(action.type){
         case CORRECTTYPE:
-            return { quizSet:state.quizSet, wordLocation:state.wordLocation+1, quizNumber:state.quizNumber }
+            const wordLocation = state.wordLocation+1
+            state.quizSetBlank[state.quizNumber] = state.quizSet[state.quizNumber].substring(0, wordLocation) + '_'.repeat(state.quizSet[state.quizNumber].length - wordLocation);
+            return { quizSet:state.quizSet, quizSetBlank:state.quizSetBlank, wordLocation:wordLocation, quizNumber:state.quizNumber }
+
         case NEXTQUIZ:
-            return { quizSet:state.quizSet, wordLocation:0, quizNumber:state.quizNumber+1 }
+            clearQuizSetBlank()
+            return { quizSet:state.quizSet, quizSetBlank:state.quizSetBlank, wordLocation:1, quizNumber:state.quizNumber+1 }
+
         case RESETQUIZ:
-            return { quizSet:state.quizSet, wordLocation:0, quizNumber:0 }
+            clearQuizSetBlank()
+            return { quizSet:state.quizSet, quizSetBlank:state.quizSetBlank, wordLocation:1, quizNumber:0 }
+
         default:
             return state
     }
